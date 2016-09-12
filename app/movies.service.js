83afebe3b678722bcc0e8760b374eceed5308c7e
Daniel Blancas <edblancas@gmail.com>
2016-09-12 03:43:15 -0500

    Fix style

--- app/movies.service.js
+++ app/movies.service.js
@@ -9,6 +9,6 @@ angular.module('movies', [])
         }
 
         this.getMovies = function () {
-            return $http.get(fileUrl).then(extract);
+            return $http.get(restUrl).then(extract);
         }
     });
