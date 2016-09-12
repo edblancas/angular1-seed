577effc34b9a35b2d59507f7d516a485ea939e6d
Daniel Blancas <edblancas@gmail.com>
2016-09-12 02:00:14 -0500

    Agrega servicio, faltaban archivos XD

---
+++ app/movies.service.js
@@ -0,0 +1,14 @@
+angular.module('movies', [])
+    .service('moviesService', function ($http) {
+        var movies,
+            restUrl = "http://localhost:8080/movies",
+            fileUrl = "data/movies.json";
+
+        function extract(result) {
+            return result.data;
+        }
+
+        this.getMovies = function () {
+            return $http.get(fileUrl).then(extract);
+        }
+    });
