6b6d159dccf5f4248a588a0d76781d13aefb2fe8
Daniel Blancas <edblancas@gmail.com>
2016-09-12 03:30:25 -0500

    Agrega alert bootstrap al boton

--- app/add-button.directive.js
+++ app/add-button.directive.js
@@ -1,7 +1,12 @@
 angular.module('directivesModule', [])
+    .controller('addMyListController', function ($scope) {
+        $scope.showAlert = function () {
+            $("#alert").fadeTo(1000, 1).slideUp();
+        }
+    })
     .directive('addMyList', function() {
         return {
             restrict: 'E',
-            template: '<button type="button" class="btn btn-default">Agregar a mi lista</button>'
+            template: '<button ng-click="showAlert()" type="button" class="btn btn-default">Agregar a mi lista</button>'
         };
     });
